0359e4fd9885051f7926ae5cce323726d1a6e8d8
sonvg <sonvg@vgroup.vn>
2016-12-01 12:20:16 +0700

    Update html

---
+++ theme/html/js/site.js
@@ -0,0 +1,47 @@
+﻿function scrollTo(div) {
+    if ($(div)) {
+        $('html, body').stop().animate({
+            scrollTop: $(div).offset().top
+        }, 1000, 'easeInOutSine', function () {
+
+        });
+    }
+}
+
+function responsive() {
+
+}
+
+$(document).ready(function () {
+
+    /*========== mmenu ==========*/
+   // var $menu = $('nav#mmenu');
+   // $menu.mmenu({
+   //     dragOpen: true,
+   //     navbar: {
+   //         title: "La Vuelta"
+   //     }
+   // });
+   //var $menuApi = $("#mmenu").data("mmenu");
+   // $('#mmenu .mm-listview>li a').click(function(e) {
+   //     $menuApi.close();
+   // });
+
+    responsive();
+});
+
+$(window).load(function () {
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        margin:10,
+        navText: ['◄', '►'],
+        nav:true,
+        dots: true,
+        loop: true,
+        autoHeight:true
+    });
+});
+
+$(window).resize(function () {
+    responsive();
+});
